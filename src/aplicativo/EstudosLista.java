2d62b1770a360e9849a78db5bf8222f188b2f9d5
Agostinho-jose <fnortodx@gmail.com>
2022-08-09 10:36:55 -0300

    Estudo Lista

---
+++ src/aplicativo/EstudosLista.java
@@ -0,0 +1,29 @@
+package aplicativo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EstudosLista {
+
+	public static void main(String[] args) {
+		
+		//Declarando variáveis e atribuindo valores valores
+				String texto1= "Modelo1";
+				String texto2= "Modelo2";
+				String texto3= "Modelo3";
+				
+				//declarando interface List e implentando classe
+				List<String> lista = new ArrayList<>();
+				
+				//implementando elementos na lista
+				
+				lista.add(texto1);
+				lista.add(texto2);
+				lista.add(texto3);
+				
+				//imprimindo elementos da lista
+				System.out.println(lista);
+
+	}
+
+}
